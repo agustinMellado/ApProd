0b3cbf71badad5fed3a4f29807ead9a986819352
agustinmellado <aguxmellado@gmail.com>
2024-05-28 09:58:12 -0300

    feat: Etiquetas de estilo y nuevo archivo para el logo

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -7,7 +7,16 @@ export const metadata: Metadata={
 const AuthPage = () => {
 
     return ( 
-        <div>pagina de auth</div>
+        <div className="flex justify-center items-center md:h-[95vh]">
+            <div className="container h-[85vh] flex-col justify-center md:grid lg:grid-cols-2 lg:px-0">
+                {/*imagen */}
+                <div className="relative hidden h-full flex-col p-10 text-white lg:flex">
+                    <div className="bg-auth  absolute inset-0">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
  
