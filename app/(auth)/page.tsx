f0264bef9246af2747a47da640e7fc7b5749287f
agustinmellado <aguxmellado@gmail.com>
2024-05-28 15:36:35 -0300

    feat:Seccion de imagen de inicio

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -10,7 +10,7 @@ const AuthPage = () => {
     return ( 
         <div className="flex justify-center items-center md:h-[95vh]">
             <div className="container h-[85vh] flex-col justify-center md:grid lg:grid-cols-2 lg:px-0">
-                {/*imagen */}
+                {/* seccion imagen */}
                 <div className="relative hidden h-full flex-col p-10 text-white lg:flex">
                     <div className="bg-auth  absolute inset-0"></div>
                     <Logo/>
