8544aa8b8fa128673e317a2db1b968b5439b7116
agustinmellado <aguxmellado@gmail.com>
2024-06-04 09:26:11 -0300

    refactor: Se modificaron los estilos.

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,5 +1,6 @@
 import Logo from "@/components/logo";
 import { Metadata } from "next";
+import SignInForm from "./components/sign-in.form";
 
 export const metadata: Metadata={
     title: "Sing IN",
@@ -8,8 +9,8 @@ export const metadata: Metadata={
 const AuthPage = () => {
 
     return ( 
-        <div className="flex justify-center items-center md:h-[95vh]">
-            <div className="container h-[85vh] flex-col justify-center md:grid lg:grid-cols-2 lg:px-0">
+        <div className="flex justify-center items-center md:h-[95vh] md:px-10 lg:px-26">
+            <div className="container h-[85vh] flex-col justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
                 {/* Seccion imagen */}
                 <div className="relative hidden h-full flex-col p-10 text-white lg:flex">
                     <div className="bg-auth  absolute inset-0"></div>
@@ -23,7 +24,8 @@ const AuthPage = () => {
                 </div>
                 {/* Seccion formulario */}
                 <div className="pt-10 lg:p-8 flex items-center md:h-[70vh]">
-                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[450]">
+                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[450px]">
+                        <SignInForm/>
 
                     </div>
                 </div>
