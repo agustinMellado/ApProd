62b63eb49679204585f5ce91720efde14852d121
agustinmellado <aguxmellado@gmail.com>
2024-06-04 11:10:12 -0300

    feat: Creacion de inputs con label y link para recuperar password.

--- app/(auth)/components/sign-in.form.tsx
+++ app/(auth)/components/sign-in.form.tsx
@@ -1,34 +1,46 @@
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 const SignInForm = () => {
-    return ( 
-
-        <>
-        
-        <div className="text-center">
-            <h1 className="text-2xl font-semibold">
-                Inicio Sesion
-            </h1>
-            <p className="text-sm text-muted-foreground">
-                Ingresa tu Email y contraseña
-            </p>
-
-            <form >
-                <div className="grid gap-3">
-                    <div className="mb-2">
-                        <Input 
-                        id="email"
-                        placeholder="email@example.com"
-                        type="email"
-                        autoComplete="email" />
-                    </div>
-                </div>
-
-            </form>
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-2xl font-semibold">Inicio Sesion</h1>
+        <p className="text-sm text-muted-foreground">
+          Ingresa tu Email y contraseña
+        </p>
+      </div>
+      <form>
+        <div className="grid gap-2">
+            {/* email */}
+          <div className="mb-3">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              placeholder="email@example.com"
+              type="email"
+              autoComplete="email"
+            />
+          </div>
+            {/* password */}
+            <div className="mb-3">
+            <Label htmlFor="email">Contaseña</Label>
+            <Input
+              id="password"
+              placeholder="Ingrese su contraseña"
+              type="password"
+              autoComplete="password"
+            />
+          </div>
+          <Link
+          href="/forgot-password"
+          className="underline text-muted-foreground underline-offset-4 hover:text-primary mb-6 text-sm text-end"
+          >¿Olvidaste tu contraseña?</Link>
         </div>
-        </>
+      </form>
+    </>
+  );
+};
 
-     );
-}
- 
 export default SignInForm;
