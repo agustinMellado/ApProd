b13b456f9368c3f26627e57336f5eaa81841ded0
Agustin Mellado <aguxmellado@gmail.com>
2024-07-12 15:03:02 -0300

    feat: Instalacion de firebase y creacion de archivo contenedor de sdk

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTmlak8SzexstqYz4OQqdUgxuLhZ-O7YE",
+  authDomain: "product-admin-nextjs-477e1.firebaseapp.com",
+  projectId: "product-admin-nextjs-477e1",
+  storageBucket: "product-admin-nextjs-477e1.appspot.com",
+  messagingSenderId: "65761637956",
+  appId: "1:65761637956:web:995a7e42728b044219ae11"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
